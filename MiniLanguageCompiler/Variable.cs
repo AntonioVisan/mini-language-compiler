c560de8388a1770c5f0560316d97d3eda8446a8f
AntonioVisan <antonio.visan@student.unitbv.ro>
2026-08-08 21:55:30 +0300

    refactor(variable): remove unused using directives

--- MiniLanguageCompiler/Variable.cs
+++ MiniLanguageCompiler/Variable.cs
@@ -1,22 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MiniLanguageCompiler
+﻿namespace MiniLanguageCompiler
 {
     internal class Variable
     {
         public string name { get; set; }
         public string type { get; set; }
-
         public dynamic value { get; set; }
         
         public bool IsConst { get; set; }
 
         public bool IsParameter { get; set; }
-
-
     }
 }
