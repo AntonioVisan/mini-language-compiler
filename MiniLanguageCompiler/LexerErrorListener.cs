aafe00f81668348c944b53238c00687be8f41d53
AntonioVisan <antonio.visan@student.unitbv.ro>
2026-08-08 21:17:36 +0300

    refactor(lexer-error-listener): clean up error listener
    - remove unused using directives
    - use the correct IAntlrErrorListener<int> type
    - translate lexical error messages to English

--- MiniLanguageCompiler/LexerErrorListener.cs
+++ MiniLanguageCompiler/LexerErrorListener.cs
@@ -1,21 +1,22 @@
 ﻿using Antlr4.Runtime;
-using System;
 using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MiniLanguageCompiler
 {
     internal class LexerErrorListener : IAntlrErrorListener<int>
     {
         public List<string> errors { get; } = new List<string>();
-        public void SyntaxError(TextWriter output, IRecognizer recognizer, int offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e)
+        public void SyntaxError(
+            TextWriter output, 
+            IRecognizer recognizer, 
+            int offendingSymbol, 
+            int line, 
+            int charPositionInLine, 
+            string msg, 
+            RecognitionException e)
         {
-            errors.Add($"Eroare lexicala la linia {line}: {msg}");
+            errors.Add($"Lexical error at line {line}: {msg}");
         }
-       
     }
 }
