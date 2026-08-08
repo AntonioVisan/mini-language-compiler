067b4c49036d390d837ecb56d6e6ef7c7fba9552
AntonioVisan <antonio.visan@student.unitbv.ro>
2026-08-08 18:56:46 +0300

    refactor: update assembly metadata

--- MiniLanguageCompiler/Properties/AssemblyInfo.cs
+++ MiniLanguageCompiler/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Tema 2 LFC")]
+[assembly: AssemblyTitle("MiniLanguageCompiler")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Tema 2 LFC")]
+[assembly: AssemblyProduct("MiniLanguageCompiler")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
